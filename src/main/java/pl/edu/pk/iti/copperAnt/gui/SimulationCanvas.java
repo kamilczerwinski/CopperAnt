c5401227e98064ec141ee9375dc12f9fd8552b60
Michał Jaworek <jaworekmichal@gmail.com>
2014-11-25 18:25:55 +0100

    Canvas with right-click to add control (computer, switch, router)

---
+++ src/main/java/pl/edu/pk/iti/copperAnt/gui/SimulationCanvas.java
@@ -0,0 +1,67 @@
+package pl.edu.pk.iti.copperAnt.gui;
+
+import javafx.event.EventHandler;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Control;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.ContextMenuEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class SimulationCanvas extends Control {
+	private double nextDeviceX;
+	private double nextDeviceY;
+
+	public SimulationCanvas() {
+		setWidth(1000);
+		setHeight(1000);
+
+		// TODO ten prostokat to brzydki hack którego trzeba sie pozbyc
+		Rectangle rectangle = new Rectangle(getWidth(), getHeight());
+		rectangle.setFill(Color.WHITE);
+		rectangle.setStroke(Color.BLACK);
+		getChildren().add(rectangle);
+
+		prepareContextMenu();
+
+		setOnContextMenuRequested(new EventHandler<ContextMenuEvent>() {
+
+			@Override
+			public void handle(ContextMenuEvent event) {
+				nextDeviceX = event.getSceneX();
+				nextDeviceY = event.getSceneY();
+			}
+		});
+	}
+
+	private void prepareContextMenu() {
+		ContextMenu contextMenu = new ContextMenu();
+
+		MenuItem addComputerItem = new MenuItem("add computer");
+		addComputerItem.setOnAction(e -> add(new ComputerControl()));
+		contextMenu.getItems().add(addComputerItem);
+
+		MenuItem addRouterItem = new MenuItem("add router");
+		addRouterItem.setOnAction(e -> add(RouterControl
+				.prepareRouterWithPorts(4)));
+		contextMenu.getItems().add(addRouterItem);
+
+		MenuItem addSwitchItem = new MenuItem("add switch");
+		addSwitchItem.setOnAction(e -> add(SwitchControl
+				.prepareSwithcWithPorts(4)));
+		contextMenu.getItems().add(addSwitchItem);
+
+		// TODO
+		// MenuItem addHubItem = new MenuItem("add hub");
+		// addHubItem.setOnAction(e -> add(new ComputerControl()));
+		// contextMenu.getItems().add(addHubItem);
+
+		setContextMenu(contextMenu);
+	}
+
+	private void add(Control control) {
+		getChildren().add(control);
+		control.setLayoutX(nextDeviceX);
+		control.setLayoutY(nextDeviceY);
+	}
+}
