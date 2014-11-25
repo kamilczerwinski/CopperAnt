5b2abb6b05c52a2bd59aecb634e62487728f2dfd
Michał Jaworek <jaworekmichal@gmail.com>
2014-11-25 17:16:44 +0100

    Konstrukt z Ip w komputerze

--- src/main/java/pl/edu/pk/iti/copperAnt/network/Computer.java
+++ src/main/java/pl/edu/pk/iti/copperAnt/network/Computer.java
@@ -1,7 +1,7 @@
 package pl.edu.pk.iti.copperAnt.network;
+
 import java.util.Random;
 import java.util.UUID;
-
 
 import pl.edu.pk.iti.copperAnt.simulation.Clock;
 import pl.edu.pk.iti.copperAnt.simulation.ConstantTimeIntervalGenerator;
@@ -16,7 +16,12 @@ public class Computer implements Device {
 		this.port = new Port(this);
 		this.ip = null;
 	}
-	
+
+	public Computer(IPAddress ip) {
+		this.port = new Port(this);
+		this.ip = ip;
+	}
+
 	public Port getPort() {
 		return port;
 	}
@@ -24,12 +29,13 @@ public class Computer implements Device {
 	@Override
 	public void acceptPackage(Package pack, Port inPort) {
 		// assume is response for arp package
-		if (pack.getType() == PackageType.DHCP && this.ip == null ) {
+		if (pack.getType() == PackageType.DHCP && this.ip == null) {
 			this.ip = new IPAddress(pack.getContent());
-		} else if (pack.getType() == PackageType.ECHO_REQUEST && pack.getDestinationIP() == this.ip.toString()) {
+		} else if (pack.getType() == PackageType.ECHO_REQUEST
+				&& pack.getDestinationIP() == this.ip.toString()) {
 			// TODO: add event to pong
 		}
-		
+
 		System.out.println("Computer received package");
 
 	}
@@ -39,23 +45,24 @@ public class Computer implements Device {
 			return;
 		}
 		long time = clock.getCurrentTime();
-		Package pack = new Package(PackageType.ECHO_REQUEST, UUID.randomUUID().toString());
+		Package pack = new Package(PackageType.ECHO_REQUEST, UUID.randomUUID()
+				.toString());
 		pack.setSourceIP(this.ip.toString());
 		IPAddress dest = this.ip;
 		Random generator = new Random();
 		dest.set(generator.nextInt(4), generator.nextInt(254) + 1);
-	    pack.setDestinationIP(dest.toString());
-		ComputerSendsEvent event = new ComputerSendsEvent(time, this,
-				pack);
+		pack.setDestinationIP(dest.toString());
+		ComputerSendsEvent event = new ComputerSendsEvent(time, this, pack);
 		event.setIntervalGenerator(new ConstantTimeIntervalGenerator(10));
 		clock.addEvent(event);
 	}
-	
+
 	public void init(Clock clock) {
 		long time = clock.getCurrentTime();
 		Package pack = new Package(PackageType.DHCP, null);
 		clock.addEvent(new PortSendsEvent(time, this.port, pack));
 	}
+
 	@Override
 	public int getDelay() {
 		return 0;
