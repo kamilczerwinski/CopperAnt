aaabd110ad01b981dbdebeea5ca1abe444cede12
Odiate <makara.jan@student.pk.edu.pl>
2014-12-09 03:41:54 +0100

    cables sketch

--- src/main/java/pl/edu/pk/iti/copperAnt/network/Cable.java
+++ src/main/java/pl/edu/pk/iti/copperAnt/network/Cable.java
@@ -4,7 +4,29 @@ import javafx.scene.control.Control;
 import pl.edu.pk.iti.copperAnt.gui.CableControl;
 import pl.edu.pk.iti.copperAnt.gui.WithControl;
 
+/*																Ethernet (10Mb/s)	| FastEthernet (100Mb/s)	| GigaEthernet (1000Mb/s)
+
+Czas trwania ramki											51.2			|						5.12					|						0.512				
+64B [mikroSekundy]
+
+Średnica domeny 												25.6  		|						2.56					|						0.256		
+kolizyjnej [mikroSekundy]
+
+Średnica domeny													5000			|						500 					|							50
+kolizyjnej [metry]
+
+http://edu.pjwstk.edu.pl/wyklady/wspsi/scb/index78.html
+*/
+
 public class Cable implements WithControl {
+
+	//For ethernet
+	int cableLength = 10;
+	//Full Duplex Mode == no colision (bedzie taka opcja?)
+	Boolean fullDuplex = false;
+
+	//Bandwith unit: Megabit per second
+	int bandwidth = 10;
 
 	Port a;
 	Port b;
@@ -15,6 +37,15 @@ public class Cable implements WithControl {
 
 	public Cable() {
 		this(false);
+	}
+
+	public Cable(int cableLength) {
+		this.cableLength = cableLength;
+	}
+
+	public Cable(int cableLength, int bandwidth) {
+		this.cableLength = cableLength;
+		this.bandwidth = bandwidth;
 	}
 
 	public Cable(boolean withGui) {
@@ -116,5 +147,9 @@ public class Cable implements WithControl {
 
 	public void setControl(CableControl control) {
 		this.control = control;
+	}
+
+	public int getBandwidth() {
+		return bandwidth;
 	}
 }
